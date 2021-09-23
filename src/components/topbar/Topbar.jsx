d016c15c8ed4e409335b7f8a349dc144732fb08b
tiruarcnas <tirupatib@arcnas.com>
2021-09-23 17:27:14 +0530

    dropdownadd

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./topbar.css";
 import { NotificationsNone, Language, Settings } from "@material-ui/icons";
-//import Dropdown from 'react-bootstrap';
+import {Dropdown} from 'react-bootstrap';
 
 export default function Topbar() {
   return (
@@ -22,17 +22,16 @@ export default function Topbar() {
           <div className="topbarIconContainer">
             <Settings />
           </div>
-          {/* <Dropdown>
+          <Dropdown>
   <Dropdown.Toggle variant="success" id="dropdown-basic">
     Dropdown Button
   </Dropdown.Toggle>
-
   <Dropdown.Menu>
     <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
     <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
     <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
   </Dropdown.Menu>
-</Dropdown> */}
+</Dropdown>
           {/* <img src="https://images.pexels.com/photos/1526814/pexels-photo-1526814.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt="" className="topAvatar" /> */}
           
         </div>
