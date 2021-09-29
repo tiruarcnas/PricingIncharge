fd1148f2898a72ec44ccb52948dc7666a839ef95
tiruarcnas <tirupatib@arcnas.com>
2021-09-29 13:37:48 +0530

    backend

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -7,7 +7,7 @@ import { NotificationsNone, Language, Settings} from "@material-ui/icons";
 
 export default function Topbar() {
   return (
-    <div className="topbar">
+    <div className="topbar" >
       <div className="topbarWrapper">
         <div className="topLeft">
           <span className="logo">Inventoryadmin</span>
