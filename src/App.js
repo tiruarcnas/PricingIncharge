a50639dd65190e9b3cc493d6f8be1a2f4eb2d793
admin <sarathkumar@arcnas.com>
2021-09-30 11:22:08 +0530

    add Login (routing)

--- src/App.js
+++ src/App.js
@@ -1,55 +1,20 @@
-import Sidebar from "./components/sidebar/Sidebar";
-import Topbar from "./components/topbar/Topbar";
-import "./App.css";
-import Home from "./pages/home/Home";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import UserList from "./pages/userList/UserList";
-import User from "./pages/user/User";
-import NewUser from "./pages/newUser/NewUser";
-import ProductList from "./pages/productList/ProductList";
-import Product from "./pages/product/Product";
-import NewProduct from "./pages/newProduct/NewProduct";
-// import Login from "./components/login/login";
-import Saveproducts from "./pages/Saveproducts/Saveproducts";
-import Profile from "./pages/Profile/Profile";
-
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/login/Login';
+import Regstion from './components/login/Regstion';
+import Routing from './Routing';
 function App() {
   return (
     <Router>
-      
-      <Topbar />
-      <div className="container">
-        <Sidebar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/profile">
-            <Profile />
-          </Route>
-          <Route path="/users">
-            <UserList />
-          </Route>
-          <Route path="/user/:userId">
-            <User />
-          </Route>
-          <Route path="/newUser">
-            <NewUser />
-          </Route>
-          <Route path="/products">
-            <ProductList />
-          </Route>
-          <Route path="/saveproducts">
-            <Saveproducts />
-          </Route>
-          <Route path="/product/:productId">
-            <Product />
-          </Route>
-          <Route path="/newproduct">
-            <NewProduct />
-          </Route>
-        </Switch>
-      </div>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+        <Route exact path="/Regstion">
+          <Regstion />
+        </Route>
+        <Routing />
+      </Switch>
     </Router>
   );
 }
