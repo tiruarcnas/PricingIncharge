6cdcf42e267d63db2b5a2999339291f2494eb0d3
karthik ayyappa <karthik@arcnas.com>
2021-10-07 14:25:55 +0530

    small changes done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/login/Login';
 import Regstion from './components/login/Regstion';
-import Routing from './Routing';
+//import Routing from './Routing';
 import MiniDrawer from './components/appbar/AppBar';
 function App() {
   return (
